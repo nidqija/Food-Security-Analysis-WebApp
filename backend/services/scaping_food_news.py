f0ead58b65625114ca91cdf8e0d5f4317e8644e0
Raziq Din <ajiqdin646@gmail.com>
2026-03-13 23:26:38 +0800

    added multiple web scraping response

--- backend/services/scaping_food_news.py
+++ backend/services/scaping_food_news.py
@@ -6,7 +6,10 @@ import random
 def scrape_food_news():
      news_list  = [
           "https://www.malaymail.com/news/malaysia/2026/03/13/no-need-to-worry-malaysias-food-supply-stable-for-raya-says-mat-sabu/212502",
-          "https://www.freemalaysiatoday.com/category/highlight/2026/03/12/war-disrupts-fertiliser-supplies-puts-food-security-at-risk"
+          "https://www.freemalaysiatoday.com/category/highlight/2026/03/12/war-disrupts-fertiliser-supplies-puts-food-security-at-risk",
+          "https://thesun.my/news/malaysia-news/mco-more-than-enough-food-supply-in-sabah-jeffrey-kitingan-im7934039/",
+          "https://themalaysianreserve.com/2025/08/18/malaysia-faces-fresh-food-supply-crisis-if-extreme-heat-persists/",
+          "https://www.thestar.com.my/news/nation/2025/11/10/local-food-production-up-but-malaysia-still-import-dependent-says-mat-sabu"
      ]
 
      random_news = random.choice(news_list)
@@ -14,8 +17,12 @@ def scrape_food_news():
           "User-Agent": "Mozilla/5.0"
      }
 
-     try:
-        response = requests.get(random_news, headers=headers)
+     all_headlines = []
+
+     for url in news_list:
+
+      try:
+        response = requests.get(url, headers=headers , timeout=10)
         response.raise_for_status()  
         inspect = BeautifulSoup(response.text, "html.parser")
         container_classes = [
@@ -32,6 +39,11 @@ def scrape_food_news():
             # Try to find H1 inside the container
             header_tag = news_container.find("h1")
 
+
+        if header_tag:
+            news_title = header_tag.text.strip()
+            all_headlines.append({"url" : url, "title": news_title})
+
         # FALLBACK: If container wasn't found OR didn't have an H1
         if not header_tag:
             # Look for ANY H1 on the page (usually the headline)
@@ -46,11 +58,12 @@ def scrape_food_news():
 
 
      
-     except Exception as e:
+      except Exception as e:
           print(f"An error occurred: {e}")
 
 
-     return news_title
+     return all_headlines
+
 
 
 if __name__ == "__main__":
